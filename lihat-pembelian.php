0db9ffc640d10f8cb2634fb496996ac10f1ba93c
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2024-04-11 23:17:56 +0700

    Filtering and additional info on lihat-pembelian and lihat-penjualan
    menambahkan fitur filtering berdasarkan item name dan filter berdasarkan date. Tidak lupa menambahkan informasi tambahan dibagian card footer seperti total qty purchased and total harga barang pada lihat-pembelian. Menambahkan Informasi total profit pada lihat-penjualan.

--- lihat-pembelian.php
+++ lihat-pembelian.php
@@ -1,6 +1,50 @@
 <?php
 include 'koneksi.php';
 session_start();
+
+// Initialize total purchase and total harga_barang variables
+$total_purchase = 0;
+$total_harga_barang = 0;
+
+// Check if the form is submitted
+if(isset($_POST['filter'])) {
+    // Capture the selected item name and date range from the form
+    $item_name = $_POST['item_name'];
+    $start_date = $_POST['start_date'];
+    $end_date = $_POST['end_date'];
+
+    // Modify your SQL query to include both item name and date range filtering
+    $query = "SELECT pb.kode_pembelian, b.nama_barang, pb.quantity, pb.tanggal_pembelian, b.harga_beli
+              FROM pembelian pb
+              JOIN barang b ON pb.id_barang = b.id_barang
+              WHERE 1=1"; // Start with a condition that is always true
+
+    // Add conditions based on the selected filters
+    if (!empty($item_name)) {
+        $query .= " AND b.nama_barang = '$item_name'";
+    }
+    if (!empty($start_date) && !empty($end_date)) {
+        $query .= " AND DATE(pb.tanggal_pembelian) BETWEEN '$start_date' AND '$end_date'";
+    }
+
+    // Finalize the query
+    $query .= " ORDER BY pb.tanggal_pembelian ASC";
+} else {
+    // Default query without any filters
+    $query = "SELECT pb.kode_pembelian, b.nama_barang, pb.quantity, pb.tanggal_pembelian, b.harga_beli
+              FROM pembelian pb
+              JOIN barang b ON pb.id_barang = b.id_barang
+              ORDER BY pb.tanggal_pembelian ASC";
+}
+
+// Execute the query
+$result = mysqli_query($koneksi, $query);
+
+// Calculate total purchase and total harga_barang
+while ($data = mysqli_fetch_array($result)) {
+    $total_purchase += $data['quantity'];
+    $total_harga_barang += $data['harga_beli'] * $data['quantity'];
+}
 ?>
 
 <?php
@@ -35,8 +79,28 @@ include 'header.php';
                                     </div>
                                 </div>
 
-                                <div class="card-body table-responsive">
-                                    <table class="table table-striped">
+                                <div class="card-body table-responsive" style="height: 600px;">
+                                    <form method="post" action="">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="item_name">Item Name:</label>
+                                                <input style="margin-left: 15px;" type="text" class="form-control" id="item_name" name="item_name" value="<?php echo isset($_POST['item_name']) ? $_POST['item_name'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="start_date">Start Date:</label>
+                                                <input style="margin-left: 15px;" type="date" class="form-control" id="start_date" name="start_date" value="<?php echo isset($_POST['start_date']) ? $_POST['start_date'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="end_date">End Date:</label>
+                                                <input style="margin-left: 15px;" type="date" class="form-control" id="end_date" name="end_date" value="<?php echo isset($_POST['end_date']) ? $_POST['end_date'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label>&nbsp;</label>
+                                                <input style="margin-top: 30px; margin-left: 15px; " type="submit" class="btn btn-primary btn-md" name="filter" value="Filter">
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <table class="table table-head-fixed table-striped">
                                         <thead>
                                             <tr>
                                                 <th>No.</th>
@@ -49,16 +113,9 @@ include 'header.php';
 
                                         <tbody>
                                             <?php
-                                            // Include your database connection file
-                                            include 'koneksi.php';
-
-                                            $query = mysqli_query($koneksi, "SELECT pb.kode_pembelian, b.nama_barang, pb.quantity, pb.tanggal_pembelian
-                                                                            FROM pembelian pb
-                                                                            JOIN barang b ON pb.id_barang = b.id_barang
-                                                                            ORDER BY pb.tanggal_pembelian ASC");
-
                                             $no = 1;
-                                            while ($data = mysqli_fetch_array($query)) {
+                                            $result = mysqli_query($koneksi, $query);
+                                            while ($data = mysqli_fetch_array($result)) {
                                                 ?>
                                                 <tr>
                                                     <td><?php echo $no; ?></td>
@@ -72,7 +129,12 @@ include 'header.php';
                                             }
                                             ?>
                                         </tbody>
+                                        
                                     </table>
+                                </div>
+                                <div class="card-footer">
+                                    <h5>Total QTY Purchased: <?php echo $total_purchase; ?></h5>
+                                    <h5>Total Harga Barang: <?php echo number_format($total_harga_barang); ?></h5>
                                 </div>
                             </div>
                         </div>
--- lihat-penjualan.php
+++ lihat-penjualan.php
@@ -1,6 +1,43 @@
 <?php 
 include 'koneksi.php';
 session_start();
+
+// Initialize total profit variable
+$total_profit = 0;
+
+// Check if the form is submitted
+if(isset($_POST['filter'])) {
+    // Capture the selected item name and date range from the form
+    $item_name = $_POST['item_name'];
+    $start_date = $_POST['start_date'];
+    $end_date = $_POST['end_date'];
+
+    // Modify your SQL query to include both item name and date range filtering
+    $query = "SELECT p.kode_penjualan, b.nama_barang, p.quantity, b.harga_beli, p.tanggal_penjualan
+              FROM penjualan p
+              JOIN barang b ON p.id_barang = b.id_barang
+              WHERE 1=1"; // Start with a condition that is always true
+
+    // Add conditions based on the selected filters
+    if (!empty($item_name)) {
+        $query .= " AND b.nama_barang = '$item_name'";
+    }
+    if (!empty($start_date) && !empty($end_date)) {
+        $query .= " AND DATE(p.tanggal_penjualan) BETWEEN '$start_date' AND '$end_date'";
+    }
+
+    // Finalize the query
+    $query .= " ORDER BY p.tanggal_penjualan ASC";
+} else {
+    // Default query without any filters
+    $query = "SELECT p.kode_penjualan, b.nama_barang, p.quantity, b.harga_beli, p.tanggal_penjualan
+              FROM penjualan p
+              JOIN barang b ON p.id_barang = b.id_barang
+              ORDER BY p.tanggal_penjualan ASC";
+}
+
+// Execute the query
+$result = mysqli_query($koneksi, $query);
 ?>
 
 <?php
@@ -36,35 +73,54 @@ include 'header.php';
                                 </div>
 
                                 <div class="card-body table-responsive p-0" style="height: 600px;">
-                                    <table class="table table-head-fixed text-nowrap">
+                                    <form method="post" action="">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="item_name">Item Name:</label>
+                                                <input style="margin-left: 15px;" type="text" class="form-control" id="item_name" name="item_name" value="<?php echo isset($_POST['item_name']) ? $_POST['item_name'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="start_date">Start Date:</label>
+                                                <input style="margin-left: 15px;" type="date" class="form-control" id="start_date" name="start_date" value="<?php echo isset($_POST['start_date']) ? $_POST['start_date'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label style="margin-left: 15px;" for="end_date">End Date:</label>
+                                                <input style="margin-left: 15px;" type="date" class="form-control" id="end_date" name="end_date" value="<?php echo isset($_POST['end_date']) ? $_POST['end_date'] : ''; ?>">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label>&nbsp;</label>
+                                                <input style="margin-top: 30px; margin-left: 15px; " type="submit" class="btn btn-primary btn-md" name="filter" value="Filter">
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <table class="table table-striped table-head-fixed text-nowrap">
                                         <thead>
                                             <tr>
                                                 <th>No.</th>
                                                 <th>Kode Penjualan</th>
                                                 <th>Item</th>
                                                 <th>Quantity</th>
+                                                <th>Profit</th>
                                                 <th>Date</th>
                                             </tr>
                                         </thead>
 
                                         <tbody>
                                             <?php
-                                            // Include your database connection file
-                                            include 'koneksi.php';
-
-                                            $query = mysqli_query($koneksi, "SELECT p.kode_penjualan, b.nama_barang, p.quantity, p.tanggal_penjualan
-                                            FROM penjualan p
-                                            JOIN barang b ON p.id_barang = b.id_barang
-                                            ORDER BY p.tanggal_penjualan ASC");
-
                                             $no = 1;
-                                            while ($data = mysqli_fetch_array($query)) {
+                                            while ($data = mysqli_fetch_array($result)) {
+                                                $purchase_price = $data['harga_beli'];
+                                                $quantity = $data['quantity'];
+                                                $profit = $quantity * $purchase_price;
+                                                // Add profit to total profit
+                                                $total_profit += $profit;
                                                 ?>
                                                 <tr>
                                                     <td><?php echo $no; ?></td>
                                                     <td><?php echo $data['kode_penjualan']; ?></td>
                                                     <td><?php echo $data['nama_barang']; ?></td>
                                                     <td><?php echo $data['quantity']; ?></td>
+                                                    <td><?php echo number_format($profit); ?></td>
                                                     <td><?php echo $data['tanggal_penjualan'] ? date("Y-m-d H:i:s", strtotime($data['tanggal_penjualan'])) : ''; ?></td>
                                                 </tr>
                                                 <?php
@@ -73,6 +129,9 @@ include 'header.php';
                                             ?>
                                         </tbody>
                                     </table>
+                                </div>
+                                <div class="card-footer">
+                                    <h5>Total Profit: <?php echo number_format($total_profit); ?></h5>
                                 </div>
                             </div>
                         </div>
