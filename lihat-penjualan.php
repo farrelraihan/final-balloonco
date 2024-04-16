aad4da5e839a9f92b96c86db1538af800043e050
Farrel Alifah Raihan <113604965+farrelraihan@users.noreply.github.com>
2024-04-16 23:02:53 +0700

    Update lihat-penjualan.php
    Adding Pagination for the table. Adjusting the Total Profit so that it seperate the current page only and the Total as a whole.

--- lihat-penjualan.php
+++ lihat-penjualan.php
@@ -2,8 +2,9 @@
 include 'koneksi.php';
 session_start();
 
-// Initialize total profit variable
-$total_profit = 0;
+// Initialize total profit variables
+$total_profit_all_pages = 0;
+$total_profit_current_page = 0;
 
 // Check if the form is submitted
 if(isset($_POST['filter'])) {
@@ -36,8 +37,27 @@ if(isset($_POST['filter'])) {
               ORDER BY p.tanggal_penjualan ASC";
 }
 
+// Pagination
+$total_records_query = "SELECT COUNT(*) AS total_records FROM penjualan";
+$total_records_result = mysqli_query($koneksi, $total_records_query);
+$total_records_row = mysqli_fetch_assoc($total_records_result);
+$total_records = $total_records_row['total_records'];
+
+$total_pages = ceil($total_records / 7); // Limit of 7 data per page
+
+$current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+$offset = ($current_page - 1) * 7;
+
+$query .= " LIMIT 7 OFFSET $offset";
+
 // Execute the query
 $result = mysqli_query($koneksi, $query);
+
+// Calculate total profit across all pages
+$total_profit_all_pages_query = "SELECT SUM(p.quantity * b.harga_beli) AS total_profit FROM penjualan p JOIN barang b ON p.id_barang = b.id_barang";
+$total_profit_all_pages_result = mysqli_query($koneksi, $total_profit_all_pages_query);
+$total_profit_all_pages_row = mysqli_fetch_assoc($total_profit_all_pages_result);
+$total_profit_all_pages = $total_profit_all_pages_row['total_profit'];
 ?>
 
 <?php
@@ -107,20 +127,20 @@ include 'header.php';
 
                                         <tbody>
                                             <?php
-                                            $no = 1;
+                                            $no = ($current_page - 1) * 7 + 1;
                                             while ($data = mysqli_fetch_array($result)) {
                                                 $purchase_price = $data['harga_beli'];
                                                 $quantity = $data['quantity'];
                                                 $profit = $quantity * $purchase_price;
                                                 // Add profit to total profit
-                                                $total_profit += $profit;
+                                                $total_profit_current_page += $profit;
                                                 ?>
                                                 <tr>
                                                     <td><?php echo $no; ?></td>
                                                     <td><?php echo $data['kode_penjualan']; ?></td>
                                                     <td><?php echo $data['nama_barang']; ?></td>
                                                     <td><?php echo $data['quantity']; ?></td>
-                                                    <td><?php echo number_format($profit); ?></td>
+                                                    <td><p>Rp<?php echo number_format($profit); ?></p></td>
                                                     <td><?php echo $data['tanggal_penjualan'] ? date("Y-m-d H:i:s", strtotime($data['tanggal_penjualan'])) : ''; ?></td>
                                                 </tr>
                                                 <?php
@@ -131,7 +151,24 @@ include 'header.php';
                                     </table>
                                 </div>
                                 <div class="card-footer">
-                                    <h5>Total Profit: <?php echo number_format($total_profit); ?></h5>
+                                    <div class="row">
+                                        <div class="col">
+                                            <h5>Total Profit (Current Page): Rp<?php echo number_format($total_profit_current_page); ?></h5>
+                                        </div>
+                                        <div class="col">
+                                            <h5>Total Profit (All Pages): Rp<?php echo number_format($total_profit_all_pages); ?></h5>
+                                        </div>
+                                    </div>
+                                    <div class="pagination">
+                                        <?php if ($current_page > 1): ?>
+                                            <a href="?page=<?php echo ($current_page - 1); ?>">Previous</a>&nbsp;&nbsp;  <?php endif; ?>
+
+                                        <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                                            <a href="?page=<?php echo $i; ?>" <?php if ($i == $current_page) echo 'class="active"'; ?>><?php echo $i; ?></a>&nbsp;&nbsp; <?php endfor; ?>
+
+                                        <?php if ($current_page < $total_pages): ?>
+                                            <a href="?page=<?php echo ($current_page + 1); ?>">Next</a>&nbsp;&nbsp;  <?php endif; ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
